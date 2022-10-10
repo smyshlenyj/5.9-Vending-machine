0343d1b66c8d1e1fce7247391911cdf99a46670e
unknown <smyshlenyj@gmail.com>
2022-10-10 22:10:42 +0300

    warning while inserting wrong product

--- main.cpp
+++ main.cpp
@@ -7,15 +7,21 @@ int main()
 	setlocale(LC_ALL, "");
 
 	// creating new snacks
+
 	Snack* snickers = new Snack("Snickers", 100, 77);
 	Snack* mars = new Snack("Mars", 100, 77);
 	
 	// testing Snack getters and setters
+	std::cout << "testing Snack getters and setters" << std::endl;
 	std::cout << "Цена " << mars->getName() << " = " << mars->getPrice() << std::endl;
 	mars->setPrice(88);
 	std::cout << "Цена " << mars->getName() << " = " << mars->getPrice() << std::endl;
-	
+	std::cout << std::endl;
+	//hi
+
+
 	// creating new slots and filling them with snacks
+	std::cout << "creating new slots and filling them with snacks" << std::endl;
 	Slot* snackSlot = new Slot(0,15);
 	snackSlot->AddSnack(*snickers);
 	snackSlot->AddSnack(*snickers);
@@ -24,33 +30,42 @@ int main()
 	snackSlot->AddSnack(*snickers);
 	snackSlot->AddSnack(*snickers);
 	snackSlot->AddSnack(*snickers);
+	snackSlot->AddSnack(*mars);
 	snackSlot->AddSnack(*snickers);
 	snackSlot->AddSnack(*snickers);
 	snackSlot->AddSnack(*snickers);
 	snackSlot->AddSnack(*snickers);
+	std::cout << std::endl;
 
 	Slot* snackSlot2 = new Slot(1, 10);
 	snackSlot2->AddSnack(*mars);
 	snackSlot2->AddSnack(*mars);
 	snackSlot2->AddSnack(*mars);
+	snackSlot2->AddSnack(*snickers);
 	snackSlot2->AddSnack(*mars);
-	
+	std::cout << std::endl;
+
 	// creating new vending machine and filling it with loaded slots
+	std::cout << "creating new vending machine and filling it with loaded slots" << std::endl;
 	VendingMachine* machine = new VendingMachine(1, 5);
 	machine->AddSlot(*snackSlot);
 	machine->AddSlot(*snackSlot2);
+	std::cout << std::endl;
 
 	// testing VendingMachine getters
+	std::cout << "testing VendingMachine getters" << std::endl;
 	std::cout << "Пустых слотов, шт.: " << machine->getEmptySlotsCount() << std::endl;
 	std::cout << "В вендинговый аппарат ID_" << machine->getId() << " загружено " << machine->getLevelOfLoad() << " товаров\n";
+	std::cout << std::endl;
 
-	// try to sell snack and check whether anything has changed
+	// trying to sell snack and check whether anything has changed
+	std::cout << "trying to sell snack and check whether anything has changed" << std::endl;
 	machine->sellSnack(0);
 	machine->sellSnack(0);
 	machine->sellSnack(0);
 	machine->sellSnack(0);
 	std::cout << "В вендинговый аппарат ID_" << machine->getId() << " загружено " << machine->getLevelOfLoad() << " товаров\n";
-
+	std::cout << std::endl;
 
 	delete snickers;
 	delete mars;
--- slot.cpp
+++ slot.cpp
@@ -11,14 +11,16 @@
 
 	void Slot::AddSnack(Snack& other)
 	{
-		if (this->capacity >= this->loadedQuantity + 1)
+		if (this->capacity >= this->loadedQuantity + 1 && ((loadedSnacks.size() > 0 && this->loadedSnacks[0].getName() == other.getName()) || loadedQuantity == 0))
 		{
 			this->loadedQuantity += 1;
 			this->freeSpace = this->capacity - this->loadedQuantity;
 			this->loadedSnacks.push_back(other);
-			std::cout << "В слот " << this->id << " загружена 1 шт. " << this->loadedSnacks.back().getName() << 
+			std::cout << "В слот " << this->id << " загружена 1 шт. " << this->loadedSnacks.back().getName() <<
 				", осталось свободного места : " << this->freeSpace << std::endl;
 		}
+		else if (loadedSnacks.size() > 0 && this->loadedSnacks[0].getName() != other.getName())
+			std::cout << "Ошибка загрузки слота - конфликт товаров!" << std::endl;
 		else
 			std::cout << "Попытка загрузить больше, чем доступно свободного места" << std::endl;
 	}
